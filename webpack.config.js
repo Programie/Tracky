98f1f963b5cd14bbb44b93fc284e2088d726874b
Michael Wieland <git@selfcoders.com>
2023-10-30 21:25:39 +0100

    Use bootstrap classes for history styling, use main entrypoint for it

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ const scriptRoot = "./src/main/resources/assets/script";
 module.exports = {
     entry: {
         "main": `${scriptRoot}/main.ts`,
-        "history": `${scriptRoot}/history.ts`,
         "error-page": `${scriptRoot}/error-page.ts`
     },
     output: {
