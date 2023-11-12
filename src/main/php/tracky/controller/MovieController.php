057ab45b1741006bdc35aa4d994ecddb036483f7
Michael Wieland <git@selfcoders.com>
2023-11-12 16:42:19 +0100

    Implemented movie detail page

--- src/main/php/tracky/controller/MovieController.php
+++ src/main/php/tracky/controller/MovieController.php
@@ -4,6 +4,7 @@ namespace tracky\controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use tracky\model\Movie;
 use tracky\orm\MovieRepository;
 
 class MovieController extends AbstractController
@@ -19,6 +20,14 @@ class MovieController extends AbstractController
     {
         return $this->render("movies.twig", [
             "movies" => $this->movieRepository->findAll()
+        ]);
+    }
+
+    #[Route("/movies/{movie}", name: "moviePage")]
+    public function getMoviePage(Movie $movie): Response
+    {
+        return $this->render("movie.twig", [
+            "movie" => $movie
         ]);
     }
 }
