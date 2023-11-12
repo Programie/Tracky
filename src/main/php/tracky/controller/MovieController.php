de4bc6cda0d4c714964f7d2beac4e969fa763912
Michael Wieland <git@selfcoders.com>
2023-11-12 16:57:40 +0100

    Implemented adding movie views in frontend

--- src/main/php/tracky/controller/MovieController.php
+++ src/main/php/tracky/controller/MovieController.php
@@ -1,16 +1,22 @@
 <?php
 namespace tracky\controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use tracky\datetime\DateTime;
 use tracky\model\Movie;
+use tracky\model\MovieView;
 use tracky\orm\MovieRepository;
+use tracky\orm\ViewRepository;
 
 class MovieController extends AbstractController
 {
     public function __construct(
-        private readonly MovieRepository $movieRepository
+        private readonly MovieRepository $movieRepository,
+        private readonly ViewRepository  $viewRepository
     )
     {
     }
@@ -29,5 +35,50 @@ class MovieController extends AbstractController
         return $this->render("movie.twig", [
             "movie" => $movie
         ]);
+    }
+
+    #[Route("/movies/{movie}/views", name: "addMovieView", methods: ["POST"])]
+    #[IsGranted("IS_AUTHENTICATED")]
+    public function addView(Movie $movie, EntityManagerInterface $entityManager): Response
+    {
+        $movieView = new MovieView;
+        $movieView->setMovie($movie);
+        $movieView->setUser($this->getUser());
+        $movieView->setDateTime(new DateTime);
+
+        $entityManager->persist($movieView);
+        $entityManager->flush();
+
+        return new Response("View added to database");
+    }
+
+    #[Route("/movies/{movie}/views/all", name: "removeMovieViews", methods: ["DELETE"])]
+    #[IsGranted("IS_AUTHENTICATED")]
+    public function removeViewsByEpisode(Movie $movie, EntityManagerInterface $entityManager): Response
+    {
+        $views = $movie->getViewsForUser($this->getUser());
+
+        foreach ($views as $view) {
+            $entityManager->remove($view);
+        }
+
+        $entityManager->flush();
+
+        return new Response("Views removed from database");
+    }
+
+    #[Route("/movies/{movie}/views/{entryId}", name: "removeMovieViewById", methods: ["DELETE"])]
+    #[IsGranted("IS_AUTHENTICATED")]
+    public function removeViewById(Movie $movie, int $entryId, EntityManagerInterface $entityManager): Response
+    {
+        $view = $this->viewRepository->findOneBy(["id" => $entryId, "user" => $this->getUser()], type: "movie");
+        if ($view === null) {
+            throw new NotFoundHttpException("View not found");
+        }
+
+        $entityManager->remove($view);
+        $entityManager->flush();
+
+        return new Response("View removed from database");
     }
 }
--- src/main/resources/assets/script/main.ts
+++ src/main/resources/assets/script/main.ts
@@ -2,15 +2,27 @@ import "./common";
 import "./history";
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll(".add-episode-view").forEach((element: HTMLElement) => {
+    document.querySelectorAll(".add-view").forEach((element: HTMLElement) => {
         element.addEventListener("click", () => {
             let dataSet = element.dataset;
 
-            fetch(`/shows/${dataSet.showId}/seasons/${dataSet.season}/episodes/${dataSet.episode}/views`, {
-                method: "POST"
-            }).then(() => {
-                document.location.reload();
-            })
+            switch (dataSet.type) {
+                case "episode":
+                    fetch(`/shows/${dataSet.showId}/seasons/${dataSet.season}/episodes/${dataSet.episode}/views`, {
+                        method: "POST"
+                    }).then(() => {
+                        document.location.reload();
+                    });
+                    break;
+
+                case "movie":
+                    fetch(`/movies/${dataSet.movieId}/views`, {
+                        method: "POST"
+                    }).then(() => {
+                        document.location.reload();
+                    });
+                    break;
+            }
         });
     });
 });
