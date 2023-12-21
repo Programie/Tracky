4fe1fa0b4533eb00b179d6b4ea2a2cfa3231b2ca
Michael Wieland <git@selfcoders.com>
2023-12-21 22:58:32 +0100

    Use fetch-movie-data and fetch-show-data command to update single movies/shows

--- src/main/php/tracky/console/FetchMovieDataCommand.php
+++ src/main/php/tracky/console/FetchMovieDataCommand.php
@@ -4,12 +4,13 @@ namespace tracky\console;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use tracky\dataprovider\Helper;
 use tracky\dataprovider\Provider;
-use tracky\model\Movie;
 use tracky\orm\MovieRepository;
+use UnexpectedValueException;
 
 #[AsCommand(name: "fetch-movie-data", description: "Fetch movie data from the configured data provider")]
 class FetchMovieDataCommand extends Command
@@ -27,19 +28,26 @@ class FetchMovieDataCommand extends Command
         $this->dataProvider = $dataProviderHelper->getProviderByType(Helper::TYPE_MOVIE);
     }
 
+    protected function configure(): void
+    {
+        $this->addArgument("movie", InputArgument::REQUIRED, "The movie ID");
+    }
+
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        /**
-         * @var $movie Movie
-         */
-        foreach ($this->movieRepository->findAll() as $movie) {
-            $output->writeln(sprintf("Fetching data for movie: %s", $movie->getTitle()));
+        $movieId = (int)$input->getArgument("movie");
 
-            $this->dataProvider->fetchMovie($movie);
-
-            $this->entityManager->persist($movie);
-            $this->entityManager->flush();
+        $movie = $this->movieRepository->findOneBy(["id" => $movieId]);
+        if ($movie === null) {
+            throw new UnexpectedValueException(sprintf("Movie with ID %d not found", $movieId));
         }
+
+        $output->writeln(sprintf("Fetching data for movie %d: %s", $movieId, $movie->getTitle()));
+
+        $this->dataProvider->fetchMovie($movie);
+
+        $this->entityManager->persist($movie);
+        $this->entityManager->flush();
 
         $output->writeln("Done");
 
--- src/main/php/tracky/console/FetchShowDataCommand.php
+++ src/main/php/tracky/console/FetchShowDataCommand.php
@@ -4,12 +4,13 @@ namespace tracky\console;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use tracky\dataprovider\Helper;
 use tracky\dataprovider\Provider;
-use tracky\model\Show;
 use tracky\orm\ShowRepository;
+use UnexpectedValueException;
 
 #[AsCommand(name: "fetch-show-data", description: "Fetch show data from the configured data provider")]
 class FetchShowDataCommand extends Command
@@ -27,19 +28,26 @@ class FetchShowDataCommand extends Command
         $this->dataProvider = $dataProviderHelper->getProviderByType(Helper::TYPE_SHOW);
     }
 
+    protected function configure(): void
+    {
+        $this->addArgument("show", InputArgument::REQUIRED, "The show ID");
+    }
+
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        /**
-         * @var $show Show
-         */
-        foreach ($this->showRepository->findAll() as $show) {
-            $output->writeln(sprintf("Fetching data for show: %s", $show->getTitle()));
+        $showId = (int)$input->getArgument("show");
 
-            $this->dataProvider->fetchShow($show, true);
-
-            $this->entityManager->persist($show);
-            $this->entityManager->flush();
+        $show = $this->showRepository->findOneBy(["id" => $showId]);
+        if ($show === null) {
+            throw new UnexpectedValueException(sprintf("Show with ID %d not found", $showId));
         }
+
+        $output->writeln(sprintf("Fetching data for show %d: %s", $showId, $show->getTitle()));
+
+        $this->dataProvider->fetchShow($show, true);
+
+        $this->entityManager->persist($show);
+        $this->entityManager->flush();
 
         $output->writeln("Done");
 
