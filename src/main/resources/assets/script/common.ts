203751384559f8f94c5d849a42d725fb220d479f
Michael Wieland <git@selfcoders.com>
2023-12-22 18:14:21 +0100

    Show images fullscreen on click

--- src/main/resources/assets/script/common.ts
+++ src/main/resources/assets/script/common.ts
@@ -5,6 +5,7 @@ import {Tooltip} from "bootstrap";
 
 import "./popper";
 import "../style/main.scss";
+import "../style/image-modal.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll("[title]").forEach(element => {
@@ -12,6 +13,40 @@ document.addEventListener("DOMContentLoaded", () => {
 
         element.addEventListener("click", () => {
             tooltip.hide();
+        });
+    });
+
+    document.querySelectorAll(".scaled-image, img").forEach((element: HTMLElement) => {
+        element.addEventListener("click", () => {
+            if (element.closest("a")) {
+                return;
+            }
+
+            let imageUrl;
+
+            if (element instanceof HTMLImageElement) {
+                imageUrl = element.src;
+            } else if (element.dataset.image) {
+                imageUrl = element.dataset.image;
+            } else {
+                imageUrl = element.style.backgroundImage.match(/url\(["']?([^"']*)["']?\)/)[1];
+            }
+
+            if (imageUrl === null || imageUrl === undefined || imageUrl === "") {
+                return;
+            }
+
+            let imageModal = document.querySelector("#image-modal") as HTMLElement;
+            let imageElement = imageModal.querySelector("img");
+
+            imageModal.classList.add("show");
+            imageElement.src = imageUrl;
+        });
+    });
+
+    document.querySelectorAll(".image-modal").forEach((element: HTMLElement) => {
+        element.addEventListener("click", () => {
+            (element.closest(".image-modal") as HTMLElement).classList.remove("show");
         });
     });
 });
