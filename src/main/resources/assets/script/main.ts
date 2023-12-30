30d2d96fda5a3324912fd7de969eb831017a6bf8
Michael Wieland <git@selfcoders.com>
2023-12-30 16:35:00 +0100

    Lazy load images

--- src/main/resources/assets/script/main.ts
+++ src/main/resources/assets/script/main.ts
@@ -45,4 +45,18 @@ document.addEventListener("DOMContentLoaded", () => {
             });
         });
     });
+
+    let lazyBackgroundObserver = new IntersectionObserver((entries) => {
+        entries.forEach(function (entry) {
+            if (entry.isIntersecting) {
+                let element = entry.target as HTMLElement;
+                element.style.backgroundImage = `url(${element.dataset.imageUrl})`;
+                lazyBackgroundObserver.unobserve(element);
+            }
+        });
+    });
+
+    document.querySelectorAll(".lazy-background").forEach(function (lazyBackground) {
+        lazyBackgroundObserver.observe(lazyBackground);
+    });
 });
