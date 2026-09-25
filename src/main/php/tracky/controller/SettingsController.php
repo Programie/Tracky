cedde3075dcacb21441f6e138d5f8206f81a0756
Michael Wieland <git@selfcoders.com>
2026-09-25 17:57:32 +0200

    Implement ability to change own password in user settings

--- src/main/php/tracky/controller/SettingsController.php
+++ src/main/php/tracky/controller/SettingsController.php
@@ -4,12 +4,19 @@ namespace tracky\controller;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use tracky\model\UserSetting;
 
 class SettingsController extends AbstractController
 {
+    public function __construct(
+        private readonly int $minPasswordLength
+    )
+    {
+    }
+
     #[Route("/settings", name: "settings_page")]
     #[IsGranted("IS_AUTHENTICATED")]
     public function getSettingsPage(): Response
@@ -33,12 +40,12 @@ class SettingsController extends AbstractController
             }
 
             if (!$option->isValid($request->request)) {
-                return $this->redirectToRoute("settings_page", ["flash" => "error", "error" => "invalid", "option" => $option->getName()]);
+                return $this->redirectToRoute("settings_page", ["flash" => "error", "error" => "save-settings", "option" => $option->getName()]);
             }
 
             $value = $option->getSettingValueFromInputBag($request->request);
             if ($value === null) {
-                return $this->redirectToRoute("settings_page", ["flash" => "error", "error" => "null", "option" => $option->getName()]);
+                return $this->redirectToRoute("settings_page", ["flash" => "error", "error" => "save-settings", "option" => $option->getName()]);
             }
 
             $setting = $option->getSetting();
@@ -69,6 +76,38 @@ class SettingsController extends AbstractController
 
         $entityManager->flush();
 
-        return $this->redirectToRoute("settings_page", ["flash" => "success"]);
+        return $this->redirectToRoute("settings_page", ["flash" => "success", "action" => "save-settings"]);
+    }
+
+    #[Route("/settings/change-password", name: "settings_change_password_action", methods: ["POST"])]
+    #[IsGranted("IS_AUTHENTICATED")]
+    public function changePassword(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = $this->getUser();
+
+        if (!$passwordHasher->isPasswordValid($user, $request->request->getString("current-password"))) {
+            return $this->redirectToRoute("settings_page", ["flash" => "error", "error" => "invalid-current-password"]);
+        }
+
+        $newPassword = $request->request->getString("new-password");
+        $newPasswordConfirm = $request->request->getString("new-password-confirm");
+
+        if (trim($newPassword) === "") {
+            return $this->redirectToRoute("settings_page", ["flash" => "error", "error" => "new-password-missing"]);
+        }
+
+        if ($newPassword !== $newPasswordConfirm) {
+            return $this->redirectToRoute("settings_page", ["flash" => "error", "error" => "new-passwords-do-not-match"]);
+        }
+
+        if (mb_strlen($newPassword) < $this->minPasswordLength) {
+            return $this->redirectToRoute("settings_page", ["flash" => "error", "error" => "new-password-too-short"]);
+        }
+
+        $user->setPassword($passwordHasher->hashPassword($user, $newPassword));
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute("settings_page", ["flash" => "success", "action" => "update-password"]);
     }
 }
